44d070e4f0988041c645ad40a04e9b6869b1fd5c
Oleksandr <59727267+okrasnoporov-clgx@users.noreply.github.com>
2025-08-25 16:27:58 +0300

    Update sort.py bubble

--- python/sort.py
+++ python/sort.py
@@ -20,6 +20,11 @@ def choise_sort(A):
 
 def bubble_sort(A):
   """ bubble sort """
+  N = len(A)
+  for bypass in range(1, N):
+    for k in range(0, N-bypass):
+      if A[k] > A[k+1]: 
+        A[k], A[k+1] = A[k+1], A[k]
   pass
 
 def test_sort (sort_algorithm):
