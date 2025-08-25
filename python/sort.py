be3e9804fe93dbdb17d14feddbae9acb59d33338
Oleksandr <59727267+okrasnoporov-clgx@users.noreply.github.com>
2025-08-25 16:22:34 +0300

    Update sort.py insert

--- python/sort.py
+++ python/sort.py
@@ -1,5 +1,12 @@
 def insert_sort(A):
   """ insert sort """
+  N = len(A)
+  for top in range (1, N):
+    k = top
+    while k > 0 and A[k-1] > A[k]:
+      A[k], A[k-1] = A[k-1], A[k]
+      k -= 1
+      
   pass
 
 def choise_sort(A):
