6ba4a34e9a63d7a6175b4e65d2647f5ca13ae30e
Oleksandr <59727267+okrasnoporov-clgx@users.noreply.github.com>
2025-08-25 15:17:49 +0300

    Update sort.py

--- python/sort.py
+++ python/sort.py
@@ -11,9 +11,22 @@ def bubble_sort(A):
   pass
 
 def test_sort (sort_algorithm):
+  print("Testing: ", sort_algorithm.__doc__)
   print("testCase #1: ", end="")
   A = [4, 2, 5, 1, 3]
   A_sorted = [1, 2, 3, 4, 5]
+  sort_algorithm(A)
+  print("OK" if A == A_sorted else "Fail")
+
+  print("testCase #2: ", end="")
+  A = list(range(10. 20)) + list(range(0, 10))
+  A_sorted = list(range(20)
+  sort_algorithm(A)
+  print("OK" if A == A_sorted else "Fail")
+
+  print("testCase #3: ", end="")
+  A = [4, 2, 4, 2, 1]
+  A_sorted = [1, 2, 2, 4, 4]
   sort_algorithm(A)
   print("OK" if A == A_sorted else "Fail")
   
@@ -21,4 +34,9 @@ def test_sort (sort_algorithm):
   #  print("Ok")
   #else:
   #  print("Fail")
-  
+
+if __name__ == "__main__":
+  test_sort(insert_sort)
+  test_sort(choise_sort)
+  test_sort(bubble_sort)
+
