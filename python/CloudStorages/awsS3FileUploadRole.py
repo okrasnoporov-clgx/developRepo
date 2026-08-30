ae967e570bd5fd2222171c599cdc6cafd15a185e
okrasnoporov <okrasnoporov@my.work>
2026-08-30 14:41:40 +0200

    aws005 add python s3 upload assumeRole

--- python/CloudStorages/awsS3FileUploadRole.py
+++ python/CloudStorages/awsS3FileUploadRole.py
@@ -8,7 +8,13 @@ file_name = sys.argv[1]
 bucket_name = os.environ["S3_BUCKET_NAME"]
 folder = os.environ["S3_FOLDER"]
 
-s3 = boto3.client("s3")
+session = boto3.Session(
+    aws_access_key_id=os.environ["AWS_ACCESS_KEY_ID"],
+    aws_secret_access_key=os.environ["AWS_SECRET_ACCESS_KEY"],
+    aws_session_token=os.environ["AWS_SESSION_TOKEN"],
+)
+
+s3 = session.client("s3")
 
 object_key = f"{folder}/{os.path.basename(file_name)}"
 
