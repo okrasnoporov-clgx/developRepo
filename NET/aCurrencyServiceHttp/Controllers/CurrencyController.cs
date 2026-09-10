b5068acf962ca3411ed94a1e0c9b2ff86fe22419
okrasnoporov <okrasnoporov@my.work>
2026-09-10 11:17:11 +0200

    add NET service HTTP app

---
+++ NET/aCurrencyServiceHttp/Controllers/CurrencyController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace aCurrencyServiceHttp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CurrencyController : ControllerBase
+    {
+
+        [HttpGet]
+        public ACurrency Get(string BankName, string CurrencyName)        
+        {
+            return GetCashRate(BankName, CurrencyName);
+        }
+
+        private string GetBankURL(string BankName) {
+
+            var config = new ConfigurationBuilder().SetBasePath(AppDomain.CurrentDomain.BaseDirectory).AddJsonFile("appsettings.json").Build();            
+            switch (BankName)
+            {
+                case "PrivatBank":
+                    return config.GetSection("PrivatBank").GetValue<String>("PRIVAT_API_JSON_CURRENCY_CASH_url");
+                case "PumbBank":
+                    return config.GetSection("PumbBank").GetValue<String>("PUMB_API_JSON_CURRENCY_CASH_url");
+                default:
+                    return BankName;
+            }
+
+        }
+        private string GetBankResponce(string BankName) {          
+
+            string api_response_data = "";
+            string Url = GetBankURL(BankName);
+            WebRequest request = WebRequest.Create(Url);
+            
+            request.Method = "GET";
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            using (StreamReader streamReader = new StreamReader(response.GetResponseStream()))
+            {
+                api_response_data = streamReader.ReadToEnd();
+                streamReader.Close();
+            }
+
+            return api_response_data;        
+        }
+        private ACurrency GetCashRate(string BankName, string CurrencyName)
+        {            
+            var currentCurrencyList = JsonSerializer.Deserialize<List<ACurrency>>(GetBankResponce(BankName));
+
+            foreach (ACurrency currency in currentCurrencyList)
+            {
+                if (currency.ccy == CurrencyName)
+                {
+                    return currency;
+
+                } 
+            } //foreach
+            return null;
+        }
+    }
+}
---
+++ NET/aCurrencyServiceHttp/Startup.cs
@@ -0,0 +1,51 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace aCurrencyServiceHttp
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddRazorPages();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                //app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                //app.UseHsts();
+            }
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapRazorPages();
+            });
+        }
+    }
+}
